b175a7ad903452117bf48533d850c1b90dc1444c
PC-08 <PC-08@DESKTOP-6SE1RJ3>
2022-10-20 12:59:42 -0500

    Suma Matriz

---
+++ src/main/java/com/mycompany/matr/Arreglo.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.matr;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC-08
+ */
+public class Arreglo {
+
+    public int[] crearVector(int f) {
+        var retorno = new int[f];
+        var lectura = new Scanner(System.in);
+        for (int i = 0; i < f; i++) {
+            System.out.println("Ingrese número en la posición: " + i);
+            retorno[i] = lectura.nextInt();
+        }
+        return retorno;
+    }
+ public int[] sumaVector(int[] v1, int[] v2) {
+        var retorno = new int[v1.length];
+        
+        for (int i = 0; i < v1.length; i++) {
+            retorno[i] = v1[i]+v2[i];
+        }
+
+        return retorno;
+    }
+    public int[][] crearMatriz(int f, int c) {
+        var retorno = new int[f][c];
+        var lectura = new Scanner(System.in);
+        var matriz = new int[f][c];
+        for (var i = 0; i < f; i++) {
+            for (var j = 0; j < c; j++) {
+                System.out.println("Ingrese el valor correspondiente a la posición [" + i + "] [" + j + "] ");
+                matriz[i][j] = lectura.nextInt();
+
+            }
+
+        }
+        return retorno;
+    }
+    
+    public int[][] sumaMatriz(int [][]m1, int [][] m2 )
+    {
+         var retorno = new int[m1.length][m1[0].length];
+         for (var i = 0; i < m1.length; i++) {
+            for (var j = 0; j < m1[0].length; j++) {
+                
+                retorno[i][j]=m1[i][j]+m2[i][j];
+           
+            }
+
+        }
+         
+         return retorno;
+         
+    }
+
+   
+
+}
--- src/main/java/com/mycompany/matr/Matr.java
+++ src/main/java/com/mycompany/matr/Matr.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.matr;
+
 import java.util.Scanner;
 
 /**
@@ -11,7 +11,39 @@ import java.util.Scanner;
  * @author PC-08
  */
 public class Matr {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
+        var arreglo = new Arreglo();
+        System.out.println("Vector 1");
+        var vector1 = arreglo.crearVector(5);
+        System.out.println("Vector 2");
+        var vector2 = arreglo.crearVector(5);
+        var vectorSumado = arreglo.sumaVector(vector1, vector2);
+
+        for (int i = 0; i < vector1.length; i++) {
+
+            System.out.println(vector1[i]);
+        }
+        for (int i = 0; i < vector2.length; i++) {
+
+            System.out.println(vector2[i]);
+        }
+              
+        System.out.println("Suma de vectores");
+        for (int i = 0; i < vector2.length; i++) {
+
+            System.out.println(vectorSumado[i]);
+        }
+        
+        
+        System.out.println("Matriz 1");
+        var matriz1 = arreglo.crearMatriz(3, 3);
+        System.out.println("Matriz 2");
+        var matriz2 = arreglo.crearMatriz(3, 3); 
+        
+        System.out.println("Suma de Matrices");
+        var sumaMatriz = arreglo.sumaMatriz(matriz1, matriz2);
+       
         /*
         var a = 0;
         System.out.println("a = " + a);
@@ -86,15 +118,16 @@ public class Matr {
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println(arreglo[i]);
         }
-        */
+         */
+ /* 
         var lectura = new Scanner(System.in);
         System.out.println("Ingrese el número de filas que tendrá la matriz");
         var filas = lectura.nextInt();
         System.out.println("Ingrese el número de columnas que tendrá la matriz");
         var columnas = lectura.nextInt();
+
+        var matriz = new int[filas][columnas];
         
-        var matriz = new int [filas][columnas];
-        /*
         System.out.println("Ingrese el valor correspondiente a la posición 0,0");
         matriz [0][0] = lectura.nextInt();
         System.out.println("Ingrese el valor correspondiente a la posición 0,1");
@@ -110,28 +143,23 @@ public class Matr {
         
         System.out.println(matriz [0][0]+" "+matriz [0][1]+" "+matriz [0][2]);
         System.out.println(matriz [1][0]+" "+matriz [1][1]+" "+matriz [1][2]);
-        */
-         for (var i = 0; i < filas; i++) {
-            for (var j = 0; j < columnas; j++){
-                System.out.println("Ingrese el valor correspondiente a la posición ["+i+"] ["+j+"] ");
-                matriz [i][j] = lectura.nextInt();
-                
-            } 
-            
-        }
-         for (var i = 0; i < filas; i++) {
-            for (var j = 0; j < columnas; j++){
-                System.out.println("El valor correspondiente a la posición ["+i+"] ["+j+"] ");
-               System.out.println(matriz[i][j]);
-                
-            } 
-            
-        }
          
-        
+        for (var i = 0; i < filas; i++) {
+            for (var j = 0; j < columnas; j++) {
+                System.out.println("Ingrese el valor correspondiente a la posición [" + i + "] [" + j + "] ");
+                matriz[i][j] = lectura.nextInt();
+
+            }
+
+        }
+        for (var i = 0; i < filas; i++) {
+            for (var j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j]);
+
+            }
+                System.out.println(" ");
+        }
+         */
     }
-    
-    
-    
-  
+
 }
